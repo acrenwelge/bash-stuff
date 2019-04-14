8bc8367a872fa0db10cb32b063ed27580a118679
Andrew Crenwelge <andrewcrenwelge@gmail.com>
2019-04-14 16:05:51 -0400

    start setup script

---
+++ setupEnv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script configures the local environment with the configuration files found in the 'config' folder:
+
+CONFIG_DIR=./config
+
+# use the first argument (but default to $HOME dir if not specified)
+INSTALL_DIR=${1:-$HOME}
+
+echo 'Remove old configuration files (y/n)? y - overwrite; n - rename old files to file.old'
+read -n 1 OVERRIDE 
+
+if [ $OVERRIDE != 'n' ]; then
+do
+  for i in .bashrc .bash .bashprofile .vimrc
+    mv $INSTALL_DIR/$i $INSTALL_DIR/$i.old
+done
+
+# Bash shell config
+cp $CONFIG_DIR/.bashrc $HOME/.bashrc
+
+# Vim config
+cp $CONFIG_DIR/.vimrc $HOME/.vimrc
